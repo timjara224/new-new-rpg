8dafc7c36e0cb812c0dd98fe0f47f4c0b7abdd76
timjara224 <william930224@gmail.com>
2025-01-10 02:33:27 +0800

    對話

--- Assets/Script/NPC/NPCcontroller.cs
+++ Assets/Script/NPC/NPCcontroller.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class NPCcontroller : MonoBehaviour, interactable
 {
+    [SerializeField] dialogs dialog;
     public void Interact()
     {
-        Debug.Log("interacting with npc");
+        Debug.Log("haha");
+        StartCoroutine(Dialog.Instance.show(dialog));
     }
 }
